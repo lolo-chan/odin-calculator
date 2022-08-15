d788573efa74ff4f49f66c5a8ce3b5a9cd758760
Rohan <rohanmchetan@gmail.com>
2022-08-15 10:30:43 +0530

    Create operate function

--- calcApp.js
+++ calcApp.js
@@ -18,3 +18,18 @@ function divide(num1, num2){
         console.log('Undefined');
     }
 }
+
+function operate(operator, num1, num2){
+    if (operator==='+'){
+        return add(num1,num2);
+    }
+    if (operator==='-'){
+        return subtract(num1,num2);
+    }
+    if (operator==='*'){
+        return multiply(num1,num2);
+    }
+    if (operator==='/'){
+        return divide(num1,num2);
+    }
+}
