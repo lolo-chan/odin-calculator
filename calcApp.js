792c735bdb58c69bdcc14c1cc36132bc18e41072
Rohan <rohanmchetan@gmail.com>
2022-08-15 12:24:22 +0530

    Add basic functionality

--- calcApp.js
+++ calcApp.js
@@ -33,3 +33,103 @@ function operate(operator, num1, num2){
         return divide(num1,num2);
     }
 }
+
+const equals = document.querySelector('.equals');
+const btnPlus = document.querySelector('.plus');
+const btnMinus = document.querySelector('.minus');
+const btnX = document.querySelector('.X');
+const btnDivide = document.querySelector('.division');
+
+const btn0 = document.querySelector('.zero');
+const btn1 = document.querySelector('.one');
+const btn2 = document.querySelector('.two');
+const btn3 = document.querySelector('.three');
+const btn4 = document.querySelector('.four');
+const btn5 = document.querySelector('.five');
+const btn6 = document.querySelector('.six');
+const btn7 = document.querySelector('.seven');
+const btn8 = document.querySelector('.eight');
+const btn9 = document.querySelector('.nine');
+const clear = document.querySelector('.clear');
+const backspace = document.querySelector('.back');
+const display = document.querySelector('.display');
+
+let num1, num2, operator;
+
+btnPlus.addEventListener('click', () => {
+    num1 = parseInt(display.textContent);
+    display.textContent = '';
+    operator = '+';
+});
+
+btnMinus.addEventListener('click', () => {
+    num1 = parseInt(display.textContent);
+    display.textContent = '';
+    operator = '-';
+});
+
+btnX.addEventListener('click', () => {
+    num1 = parseInt(display.textContent);
+    display.textContent = '';
+    operator = '*';
+});
+
+btnDivide.addEventListener('click', () => {
+    num1 = parseInt(display.textContent);
+    display.textContent = '';
+    operator = '/';
+})
+
+equals.addEventListener('click', () => {
+    num2 = parseInt(display.textContent);
+    display.textContent = operate(operator, num1, num2);
+});
+
+
+clear.addEventListener('click', () => {
+    display.textContent = '';
+});
+
+btn0.addEventListener('click', () => {
+    display.textContent += '0';
+});
+
+btn1.addEventListener('click', () => {
+    display.textContent += '1';
+});
+
+btn2.addEventListener('click', () => {
+    display.textContent += '2';
+});
+
+btn3.addEventListener('click', () => {
+    display.textContent += '3';
+});
+
+btn4.addEventListener('click', () => {
+    display.textContent += '4';
+});
+
+btn5.addEventListener('click', () => {
+    display.textContent += '5';
+});
+
+btn6.addEventListener('click', () => {
+    display.textContent += '6';
+});
+
+btn7.addEventListener('click', () => {
+    display.textContent += '7';
+});
+
+btn8.addEventListener('click', () => {
+    display.textContent += '8';
+});
+
+btn9.addEventListener('click', () => {
+    display.textContent += '9';
+});
+
+backspace.addEventListener('click', () => {
+    display.textContent = display.textContent.slice(0,-1);
+});
